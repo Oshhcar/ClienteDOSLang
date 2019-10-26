f7dae877e0fe126e2749925d678bcf76a94d22e5
Oscar Morales <oskralberto14@gmail.com>
2019-10-25 22:56:12 -0600

    Agregando funcionalidad debugger

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input, ɵConsole } from '@angular/core';
 import { FileInterface } from '../../models/file.interface';
 import { ErrorInterface } from '../../models/error.interface';
 
@@ -9,6 +9,7 @@ import 'ace-builds/src-noconflict/theme-terminal'
 import 'ace-builds/src-noconflict/mode-text';
 import { SocketService } from 'src/app/services/socket/socket.service';
 import { ContenidoInterface } from 'src/app/models/contenido.interface';
+import { isNullOrUndefined } from 'util';
 
 const THEME = 'ace/theme/github';
 const LANG = 'ace/mode/java';
@@ -21,18 +22,18 @@ const parserCuadruplos = require('../../parser/cuadruplos/parserCuadruplos');
   styleUrls: ['./editor.component.css']
 })
 export class EditorComponent implements OnInit {
-  
+
   @Input('file') file: FileInterface;
 
   @Input('files') files: FileInterface[];
 
-  @ViewChild('codeEditor', {static:true}) codeEditorElmRef: ElementRef;
+  @ViewChild('codeEditor', { static: true }) codeEditorElmRef: ElementRef;
   private codeEditor: ace.Ace.Editor;
 
-  @ViewChild('codeEditor3d', {static: true}) codeEditorElmRef3d: ElementRef;
+  @ViewChild('codeEditor3d', { static: true }) codeEditorElmRef3d: ElementRef;
   private codeEditor3d: ace.Ace.Editor;
 
-  @ViewChild('consolaEditor', {static: true}) consolaEditorElmRef: ElementRef;
+  @ViewChild('consolaEditor', { static: true }) consolaEditorElmRef: ElementRef;
   private consolaEditor: ace.Ace.Editor;
 
   constructor(private socket: SocketService) { }
@@ -53,7 +54,7 @@ export class EditorComponent implements OnInit {
     this.codeEditor.setValue(this.file.content);
     this.codeEditor.navigateLineEnd();
 
-    this.codeEditor.addEventListener("change", ()=>{
+    this.codeEditor.addEventListener("change", () => {
       this.file.content = this.codeEditor.getValue();
     });
 
@@ -69,6 +70,29 @@ export class EditorComponent implements OnInit {
     this.codeEditor3d.setTheme(THEME);
     this.codeEditor3d.getSession().setMode(LANG);
     this.codeEditor3d.setShowFoldWidgets(true);
+
+    this.codeEditor3d.addEventListener("guttermousedown", (e)=>{
+      var target = e.domEvent.target;
+
+      if (target.className.indexOf("ace_gutter-cell") == -1) {
+        return;
+      }
+      
+      var breakpoints = e.editor.session.getBreakpoints(row, 0);
+      
+      var row = e.getDocumentPosition().row;
+      
+      if(isNullOrUndefined(breakpoints[row])){
+        e.editor.session.setBreakpoint(row);
+        this.breakpoints[row+1] = 1;
+      } else {
+        e.editor.session.clearBreakpoint(row);
+        this.breakpoints[row+1] = null;
+      }
+
+      //console.log("entra " + row)
+      e.stop();
+    });
 
     const elementConsola = this.consolaEditorElmRef.nativeElement;
     const editorOptionsConsola: Partial<ace.Ace.EditorOptions> = {
@@ -86,6 +110,7 @@ export class EditorComponent implements OnInit {
 
   }
 
+
   error = '';
   mensaje = '';
   error2 = '';
@@ -94,13 +119,15 @@ export class EditorComponent implements OnInit {
   errorEditor: ErrorInterface[] = [];
   errorEjecucion: ErrorInterface[] = [];
 
-  reportes: FileInterface[]=[];
+  reportes: FileInterface[] = [];
 
   contenido: ContenidoInterface;
 
-  onGuardar(){
+  breakpoints: number[] = [];
+
+  onGuardar() {
     //console.log(this.codeEditor.getValue());
-    var blob = new Blob([this.codeEditor.getValue()], { type : 'text/plain'});
+    var blob = new Blob([this.codeEditor.getValue()], { type: 'text/plain' });
     var url = window.URL.createObjectURL(blob);
 
     let linkElement = document.createElement('a');
@@ -116,35 +143,35 @@ export class EditorComponent implements OnInit {
     linkElement.dispatchEvent(clickEvent);
   }
 
-  deleteReporte(pos: number){
-    this.reportes.splice(pos,1); 
+  deleteReporte(pos: number) {
+    this.reportes.splice(pos, 1);
   }
 
-  generarTabla(val: string, filename: string, reporte: FileInterface){
-    let id = filename + "reporte_"+ this.reportes.indexOf(reporte);
+  generarTabla(val: string, filename: string, reporte: FileInterface) {
+    let id = filename + "reporte_" + this.reportes.indexOf(reporte);
     let doc = document.getElementById(id);
     doc.innerHTML = val;
   }
 
   onCompilar() {
-    this.error= '';
+    this.error = '';
     this.mensaje = '';
     this.file.main = true;
 
     this.socket.traducir(this.files)
-    .subscribe(
-      (contenido: ContenidoInterface) =>{
-        this.contenido = contenido;
-        this.codeEditor3d.setValue(this.contenido.content.toString());
-        this.codeEditor3d.navigateLineEnd();
+      .subscribe(
+        (contenido: ContenidoInterface) => {
+          this.contenido = contenido;
+          this.codeEditor3d.setValue(this.contenido.content.toString());
+          this.codeEditor3d.navigateLineEnd();
 
-        this.errorEditor = contenido.errors;
-      },
-      (error) =>{
-        this.error = "Error de conexión, inténtelo nuevamente.";
-        console.log(error.toString());
-      }
-    );
+          this.errorEditor = contenido.errors;
+        },
+        (error) => {
+          this.error = "Error de conexión, inténtelo nuevamente.";
+          console.log(error.toString());
+        }
+      );
     //console.log("archivos->"+this.files);
 
     this.file.main = false;
@@ -160,15 +187,35 @@ export class EditorComponent implements OnInit {
 
     let ast = parserCuadruplos.parse(this.codeEditor3d.getValue());
     //ast.ejecutar(this.consolaEditor, this.errorEditor);
-    if(ast){
+    if (ast) {
       ast.ejecutar(this.consolaEditor, this.errorEjecucion);
     }
 
   }
 
-  onDepurar() {
+  async onDepurar() {
     this.error2 = '';
     this.mensaje2 = '';
+    this.codeEditor3d.setReadOnly(true);
+
+    let i = 0;
+    while (i < 33) {
+      this.codeEditor3d.gotoLine(i, 0, true);
+      this.codeEditor3d.setHighlightActiveLine(true);
+      this.codeEditor3d.focus();
+
+      if(!isNullOrUndefined(this.breakpoints[i])){
+        break;
+      }
+
+      await this.delay(1000);
+      i++;
+    }
+    this.codeEditor3d.setReadOnly(false);
+  }
+
+  delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
 }
